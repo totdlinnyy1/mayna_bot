748e7bc49cecfdf0ec6a1ddad13d9ad4de997233
tot_dlinnyy <gnngr333@gmail.com>
2022-08-05 00:41:43 +0700

    feat: add telegraf module and config file, edit .env.example file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { TelegrafModule } from 'nestjs-telegraf'
 
 import { config } from './config/config'
+import { telegrafConfig } from './config/telegraf.config'
 import { typeOrmConfig } from './config/typeorm.config'
 
 @Module({
     imports: [
         ConfigModule.forRoot(config),
         TypeOrmModule.forRootAsync(typeOrmConfig),
+        TelegrafModule.forRootAsync(telegrafConfig),
     ],
 })
 export class AppModule {}
